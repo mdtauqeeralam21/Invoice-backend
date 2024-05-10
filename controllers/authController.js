6b3e72fe8b6e737b16083f9938eccaa594c1424d
Tauqeer Alam <mdta2108@gmail.com>
2024-05-10 16:11:48 +0530

    cookies issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,39 +1,35 @@
-import User from '../models/user.js';
-import { StatusCodes } from 'http-status-codes';
-import { BadRequestError,UnAuthenticatedError } from '../errors/index.js';
-import xssFilters from 'xss-filters';
+import User from "../models/user.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import xssFilters from "xss-filters";
 
 const oneDay = 1000 * 60 * 60 * 24;
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
-
   if (!name || !email || !password) {
-    
     throw new BadRequestError("Please provide all values");
   }
 
-  const userAlreadyExists = await User.findOne({email});
+  const userAlreadyExists = await User.findOne({ email });
 
-  if(userAlreadyExists){
+  if (userAlreadyExists) {
     throw new BadRequestError(`The email: ${email} is already in use.`);
   }
-
 
   const user = await User.create({ name, email, password });
 
   const token = user.createToken();
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
   });
   res.status(StatusCodes.CREATED).json({
-    
-      email: user.email,
-      name: user.name
-    
+    email: user.email,
+    name: user.name,
   });
 };
 
@@ -44,7 +40,7 @@ const login = async (req, res) => {
     throw new BadRequestError("Please provide all values");
   }
 
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
     throw new UnAuthenticatedError("Invalid Credentials");
@@ -67,54 +63,18 @@ const login = async (req, res) => {
   }
 
   const token = user.createToken();
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
   });
-
 
   res.status(StatusCodes.OK).json({
     name: user.name,
-    email: user.email
+    email: user.email,
   });
 };
-
-
-// const login = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   if(!email || !password) {
-//     throw new BadRequestError("Please provide all values");
-//   }
-
-  
-  
-//   const user = await User.findOne({ email }).select('+password');
-
-//   if(!user) {
-//     throw new UnAuthenticatedError("Invalid Credentials");
-//   }
-
-//   const isPasswordCorrect = await user.comparePassword(password);
-
-//   if(!isPasswordCorrect) {
-//     throw new UnAuthenticatedError("Invalid Credentials");
-//   }
-
-//   const token = user.createToken();
-//   res.cookie('token', token, {
-//     httpOnly: true,
-//     expires: new Date(Date.now() + oneDay),
-//     secure: process.env.NODE_ENV === 'production',
-//   });
-
-//   user.password = undefined;
-
-//   res.status( StatusCodes.OK ).json({ 
-//     user
-//   });
-// };
 
 const updateUser = async (req, res) => {
   const { email, name } = req.body;
@@ -131,37 +91,38 @@ const updateUser = async (req, res) => {
   await user.save();
 
   const token = user.createToken();
-  
-  res.cookie('token', token, {
+
+  res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
+    sameSite: 'None',
   });
 
-  res.status(StatusCodes.OK).json({ 
+  res.status(StatusCodes.OK).json({
     name: user.name,
-    email: user.email
+    email: user.email,
   });
 };
 
 const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
-  res.status(StatusCodes.OK).json({ 
+  res.status(StatusCodes.OK).json({
     name: user.name,
     email: user.email,
-    isAdmin: user.isAdmin
+    isAdmin: user.isAdmin,
   });
 };
 
 const logout = async (req, res) => {
-  res.cookie('token', 'logout', {
+  res.cookie("token", "logout", {
     httpOnly: true,
     expires: new Date(Date.now() + 1000),
   });
 
   res.status(StatusCodes.OK).json({
-    msg: 'User logged out!'
+    msg: "User logged out!",
   });
 };
 
-export { register, login, updateUser, getCurrentUser, logout }
+export { register, login, updateUser, getCurrentUser, logout };
